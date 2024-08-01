cf5f79c0cb111be352af18a0b00274a27e5faaad
GiulioBarkee <giuliobarchi1@gmail.com>
2024-08-01 12:31:50 +0200

    versione 1

---
+++ introduction.js
@@ -0,0 +1,12 @@
+//REPLACE
+// let string = 'la vita è baddi'
+// console.log(string.replace('baddi', 'bella' ))
+
+//
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.body.innerHTML = 'cio';
+// });
+
+
+
+
